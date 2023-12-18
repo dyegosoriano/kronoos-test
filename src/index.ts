671696bbc67eaf36388ec4706cb354bd788978a3
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2023-12-18 16:09:37 -0300

    feat: fazendo leitura do arquivo

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('TESTE')
+import filesystem from 'node:fs'
+import { parse } from 'csv-parse'
+import path from 'node:path'
+
+const directory = path.resolve(__dirname, '..', 'data.csv')
+
+const stream = filesystem.createReadStream(directory)
+const parser = parse({ delimiter: ',' })
+
+stream.pipe(parser)
+
+parser.on('data', async line => console.log(line)).on('end', () => console.log('Finish!'))
